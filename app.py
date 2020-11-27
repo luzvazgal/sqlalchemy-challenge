3e78c1dc965b01bd98f792b60cfc961b0d6a065c
Luz Vazquez Galindo <luzvazgal@MacBook-Air-de-Luz.local>
2020-11-27 01:00:30 -0600

    Final app.py

--- app.py
+++ app.py
@@ -82,27 +82,29 @@ def tobs():
         #To associate dates and tobs to the station
         dates_tobs_dic = []
         #Adding station id as first element of array
-        max_temp_station = []
+        max_temp_station = {}
         #max_temp_station = [{ 'dates_tobs': dates_tobs_dic, 'station': station_id[0]}]
-        max_temp_station.append({'station': station_id[0]})
+        max_temp_station['station']= station_id[0]
 
         max_temp_station_q = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station==station_id[0])
         
+        #Adding all dates and temperatures
         for max_temp_station_row in max_temp_station_q:
                 dates_tobs_dic.append({'date': max_temp_station_row.date, 'tobs': max_temp_station_row.tobs})
 
-        max_temp_station.append(dates_tobs_dic)
+        #Adding the list of dates and tem`peratures at the same level as station 
+        max_temp_station['dates_tobs']=dates_tobs_dic
         return jsonify(max_temp_station)
 
 #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 @app.route("/api/v1.0/<start>")
 def temperature(start):
         
+        #Validating start date format
         try:
                 dt.datetime.strptime(start, '%Y-%m-%d')
-        except ValueError:
-                #raise ValueError("Incorrect date format, should be YYYY-MM-DD")
-                return jsonify({"Error":"Incorrect date format, should be YYYY-MM-DD"})
+        except ValueError:     
+                return jsonify({"Error":"Incorrect start date format, should be YYYY-MM-DD"})
 
 
         query= (session.query(Measurement.station, func.count(Measurement.station))
@@ -112,17 +114,17 @@ def temperature(start):
 
       #The most active station:
         station_id = query.first()
+             
         
-        
-        
-        #Getting max, min, average temperatures given a start date
+        #Getting max, min, average temperatures given a start date and end date
         station_temp_q = (
                 session.query(Measurement.station, func.min(Measurement.tobs).label('min_temp'), 
                 func.max(Measurement.tobs).label('max_temp'), 
                 func.avg(Measurement.tobs).label('avg_temp'),
                 Measurement.date)
                 .filter(Measurement.station==station_id[0])
-                .filter(Measurement.date>=start).first()
+                .filter(Measurement.date>=start)
+                .first()
                 )
 
         #To print results
@@ -135,6 +137,55 @@ def temperature(start):
         return jsonify(station_temp)
 
         
+#Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_2(start, end):
+        
+        #Validating start date format
+        try:
+                dt.datetime.strptime(start, '%Y-%m-%d')
+        except ValueError:     
+                return jsonify({"Error":"Incorrect start date format, should be YYYY-MM-DD"})
+
+       
+        #Validating end date format
+        try:
+                dt.datetime.strptime(end, '%Y-%m-%d')
+                       
+        except ValueError:
+                return jsonify({"Error":"Incorrect end date format, should be YYYY-MM-DD"})
+        
+
+        query= (session.query(Measurement.station, func.count(Measurement.station))
+                .group_by(Measurement.station)
+                .order_by(func.count(Measurement.station).desc())
+                )
+
+      #The most active station:
+        station_id = query.first()
+             
+        
+        #Getting max, min, average temperatures given a start date and end date
+        station_temp_q = (
+                session.query(Measurement.station, func.min(Measurement.tobs).label('min_temp'), 
+                func.max(Measurement.tobs).label('max_temp'), 
+                func.avg(Measurement.tobs).label('avg_temp'),
+                Measurement.date)
+                .filter(Measurement.station==station_id[0])
+                .filter(Measurement.date>=start)
+                .filter(Measurement.date<=end)
+                .first()
+                )
+
+        #To print results
+        station_temp = {}
+        station_temp['station'] = station_temp_q.station
+        station_temp['min_temp'] = station_temp_q.min_temp
+        station_temp['max_temp'] = station_temp_q.max_temp
+        station_temp['avg_temp'] = station_temp_q.avg_temp
+
+        return jsonify(station_temp)
+
 
 if __name__ == "__main__":
     # @TODO: Create your app.run statement here
